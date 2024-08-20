0e3f750d9d4165da94e383b327ec6b472ca43bea
fortidg <dgooch@fortinet.com>
2024-08-20 11:49:12 +0000

    Adding Log sync and updating gcp provider version.

---
+++ pub_sub.tf
@@ -0,0 +1,14 @@
+resource "google_pubsub_topic" "log_topic" {
+    name = "log-topic"
+}
+
+resource "google_logging_project_sink" "ascale-sink" {
+    name        = "autoscale-log-sink"
+    destination = "pubsub.googleapis.com/projects/${var.project_id}/topics/${google_pubsub_topic.log_topic.name}"
+    filter = <<EOF
+    protoPayload.methodName="v1.compute.instances.insert" OR "v1.compute.instances.delete"
+    protoPayload.requestMetadata.callerSuppliedUserAgent="GCE Managed Instance Group"
+    protoPayload.response.@type="type.googleapis.com/operation"
+    SEARCH("fortigateautoscale-instance-${random_string.random_name_post.result}")
+    EOF
+}
