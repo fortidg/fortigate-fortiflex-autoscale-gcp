b5c639c716ea173f86e1d243f300a390ab653d1d
fortidg <dgooch@fortinet.com>
2024-08-26 13:50:11 +0000

    added description to sink, also arranged filters

--- pub_sub.tf
+++ pub_sub.tf
@@ -4,11 +4,12 @@ resource "google_pubsub_topic" "log_topic" {
 
 resource "google_logging_project_sink" "ascale-sink" {
     name        = "autoscale-sink-${random_string.random_name_post.result}"
+    description = "Sink for autoscale logs"
     destination = "pubsub.googleapis.com/projects/${var.project}/topics/${google_pubsub_topic.log_topic.name}"
     filter = <<EOF
     protoPayload.methodName="v1.compute.instances.insert" OR "v1.compute.instances.delete"
-    protoPayload.requestMetadata.callerSuppliedUserAgent="GCE Managed Instance Group"
-    protoPayload.response.@type="type.googleapis.com/operation"
-    SEARCH("fortigateautoscale-instance-${random_string.random_name_post.result}")
+protoPayload.requestMetadata.callerSuppliedUserAgent="GCE Managed Instance Group"
+protoPayload.response.@type="type.googleapis.com/operation"
+SEARCH("fortigateautoscale-instance-${random_string.random_name_post.result}")
     EOF
 }
