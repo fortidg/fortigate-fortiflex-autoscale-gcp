3580584004497dcee42aa72c1b80bc0dd8795329
fortidg <dgooch@fortinet.com>
2024-08-26 11:10:12 +0000

    added random string to sink and log topic  increased time_sleep to 10 minutes

--- pub_sub.tf
+++ pub_sub.tf
@@ -1,9 +1,9 @@
 resource "google_pubsub_topic" "log_topic" {
-    name = "ascaler-log-topic"
+    name = "ascaler-topic-${random_string.random_name_post.result}"
 }
 
 resource "google_logging_project_sink" "ascale-sink" {
-    name        = "autoscale-log-sink"
+    name        = "autoscale-sink-${random_string.random_name_post.result}"
     destination = "pubsub.googleapis.com/projects/${var.project}/topics/${google_pubsub_topic.log_topic.name}"
     filter = <<EOF
     protoPayload.methodName="v1.compute.instances.insert" OR "v1.compute.instances.delete"
